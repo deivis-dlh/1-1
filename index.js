590eefa44f81ef4b9cf80cfcb20fb9a7efa554a6
habitsonorous <67773920+habitsonorous@users.noreply.github.com>
2020-07-03 12:06:14 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,12 +2,25 @@
 numero1 = document.getElementById("txt1").value
 umero2 = document.getElementById("txt2").value
 
-ondicion = prompt ("¿Qué desea hacer?")
+condicion = prompt ("¿Qué desea hacer?")
 
-f (condicion = sumar){
-  suma1 = (numero1 + numero2)
+if(condicion = "0"){
+  	suma1 = (numero1+numero2)
 	alert ("El resultado de la suma es: " + suma1)
 }
-else {
+else{
+	condicion1 = prompt ("¿Qué desea hacer?")
+}if(condicion1 = "1"){
+	multiplica = (numero1*numero2)
+	alert ("El resultado de la multiplicación es: " + multiplica)
+}else{ 
 	condicion2 = prompt ("¿Qué desea hacer?")
+}if(condicion2 = "2"){
+	dividir = (numero1/numero2)
+	alert ("El resultado de la divición es: " + dividir)
+}else{
+	condicion3 = prompt ("¿Qué desea hacer?")
+}if(condicion3 = "3"){
+	promedio = (numero1+numero2)/2
+	alert("El promedio es: " + promedio)
 }
