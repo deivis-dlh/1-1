e9ec8c303503d688237882aed7f9ca379ad24ccb
Daniel Mercado <60846433+Danmet24@users.noreply.github.com>
2020-07-03 12:29:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 
-numero1 = document.getElementById("txt1").value
-numero2 = document.getElementById("txt2").value
+numero1 = 2//document.getElementById("txt1").value
+numero2 = 2//document.getElementById("txt2").value
 
 condicion = prompt ("¿Qué desea hacer?")
 
-if(condicion = "sumar"){
+if(condicion = "0"){
   	suma1 = (numero1 + numero2)
 	alert ("El resultado de la suma es: " + suma1)
 }
-else if(condicion = "multiplicar"){
+else if(condicion = "1"){
 	multiplica = (numero1*numero2)
 	alert ("El resultado de la multiplicación es: " + multiplica)
 }
-else if(condicion = "dividir"){
+else if(condicion = "2"){
 	dividir = (numero1/numero2)
 	alert ("El resultado de la divición es: " + dividir)
 }
-else if(condicion = "promedio"){
+else if(condicion = "3"){
 	promedio = (numero1+numero2)/2
 	alert("El promedio es: " + promedio)
 }
