d9058b914897859244141a58cf7a3f1f27157c72
Dammet <damet.2401@gmail.com>
2020-07-04 12:24:03 -0500

    Conectando con el html

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-alert("senfdjfj")
 
-document.getElementById("send").addEventListener('click', main)
-condicion = prompt ("¿Qué desea hacer?")
+campo1 = document.getElementById('txt1')
+campo2 = document.getElementById('txt2')
+result = document.getElementById('res')
 
-numero1 = 0
-numero2 = 0
+let numero1
+let numero2
+
+boton = document.getElementById('send')
+boton.addEventListener('click', main)
 
 function main()
 {
-	numero1 = document.getElementById("txt1").value
-	numero2 = document.getElementById("txt2").value
+	numero1 = parseInt(campo1.value)
+	numero2 = parseInt(campo2.value)
+
+	condicion = document.getElementById('cars').value 
+
+	if(condicion == "0"){
+		  result.innerText = (numero1 + numero2)
+	}
+	else if(condicion == "1"){
+			 result.innerText = (numero1*numero2)
+	}
+	else if(condicion == "2"){
+			result.innerText = (numero1/numero2)
+	}
+	else if(condicion == "3"){
+			result.innerText = (numero1+numero2)/2
+	}
+	else if(condicion == "4"){
+			result.innerText = (numero1-numero2)
+	}
 }
